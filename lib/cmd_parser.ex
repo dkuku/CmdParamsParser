47d544d8e70ad48c5e9839601773c925c1dbfca3
Daniel Kukula <daniel.kuku@gmail.com>
2021-06-05 09:54:56 +0100

    refactor

--- lib/cmd_parser.ex
+++ lib/cmd_parser.ex
@@ -20,30 +20,68 @@ defmodule CmdParamsParser do
   """
 
   def parse(args, opts \\ []) do
-    {parsed, rest, err} = OptionParser.parse(args, opts)
+    {new_strict, lists} = replace_list_with_string(opts[:strict])
+
+    compatible_opts = Keyword.put(opts, :strict, new_strict)
+    {cmd_parsed, rest, err} = OptionParser.parse(args, compatible_opts)
 
     {env_parsed, _, _} =
       Enum.reduce(opts[:strict], [], fn {key, type}, acc ->
-        value =
-          key
-          |> Atom.to_string()
-          |> String.upcase()
-          |> System.get_env()
-
-        if Keyword.get(parsed, key) do
+        if Keyword.get(cmd_parsed, key) do
           acc
         else
-          case type do
-            :boolean ->
-              [if(value == "true", do: "--", else: "--no-") <> Atom.to_string(key) | acc]
-
-            _ ->
-              if value, do: ["--" <> Atom.to_string(key), value | acc], else: acc
-          end
+          convert_to_option_parser(acc, key, type)
         end
       end)
       |> OptionParser.parse(opts)
 
-    {env_parsed ++ parsed, rest, err}
+    all_parsed =
+      if length(lists) > 0 do
+        split_lists(env_parsed ++ cmd_parsed, lists)
+      else
+        env_parsed ++ cmd_parsed
+      end
+
+    {all_parsed, rest, err}
+  end
+
+  defp replace_list_with_string(params) do
+    params
+    |> Enum.reduce({[], []}, fn {key, value}, {compatible_opts, lists} ->
+      if value == :list do
+        {[{key, :string} | compatible_opts], [key | lists]}
+      else
+        {[{key, value} | compatible_opts], lists}
+      end
+    end)
+  end
+
+  def convert_to_option_parser(acc, key, type) do
+    value = get_from_env(key)
+
+    case type do
+      :boolean ->
+        [if(value == "true", do: "--", else: "--no-") <> Atom.to_string(key) | acc]
+
+      _ ->
+        if value, do: ["--" <> Atom.to_string(key), value | acc], else: acc
+    end
+  end
+
+  def get_from_env(key) do
+    key
+    |> Atom.to_string()
+    |> String.upcase()
+    |> System.get_env()
+  end
+
+  def split_lists(parsed, lists) do
+    Enum.map(parsed, fn {key, value} ->
+      if key in lists do
+        {key, String.split(value, ",")}
+      else
+        {key, value}
+      end
+    end)
   end
 end
--- test/cmd_parser_test.exs
+++ test/cmd_parser_test.exs
@@ -45,4 +45,43 @@ defmodule CmdParamsParserTest do
     assert CmdParamsParser.parse([], strict: [count: :float]) == {[count: 3.5], [], []}
     System.delete_env("COUNT")
   end
+
+  test "parses list options from options list" do
+    assert CmdParamsParser.parse(["--users", "qw,er,ty"], strict: [users: :list]) ==
+             {[users: ["qw", "er", "ty"]], [], []}
+  end
+
+  test "parses list options from system variables" do
+    System.put_env("USERS", "qw,er,ty")
+
+    assert CmdParamsParser.parse([], strict: [users: :list]) ==
+             {[users: ["qw", "er", "ty"]], [], []}
+
+    System.delete_env("USERS")
+  end
+
+  test "parses all possible options from options list" do
+    assert CmdParamsParser.parse(
+             ["--users", "qw,er,ty", "--count", "3.5", "--debug", "--val", "6"],
+             strict: [users: :list, count: :float, debug: :boolean, val: :integer]
+           ) ==
+             {[{:users, ["qw", "er", "ty"]}, {:count, 3.5}, {:debug, true}, {:val, 6}], [], []}
+  end
+
+  test "parses all possible options from system variables" do
+    System.put_env("USERS", "qw,er,ty")
+    System.put_env("COUNT", "3.5")
+    System.put_env("DEBUG", "true")
+    System.put_env("VAL", "7")
+
+    assert CmdParamsParser.parse([],
+             strict: [users: :list, count: :float, debug: :boolean, val: :integer]
+           ) ==
+             {[{:val, 7}, {:debug, true}, {:count, 3.5}, {:users, ["qw", "er", "ty"]}], [], []}
+
+    System.delete_env("USERS")
+    System.delete_env("COUNT")
+    System.delete_env("DEBUG")
+    System.delete_env("VAL")
+  end
 end
